a149823041f402acca2c3a9b1918f7449a76af9c
MUHAMMAD USAMA <Mubhanbhro@gmail.com>
2026-07-30 07:41:45 +0500

    Use hash location for WouterRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Router as WouterRouter, Switch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
 function Router() {
   return (
-    <WouterRouter base="/portfolio">
+    <WouterRouter hook={useHashLocation}>
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/404" component={NotFound} />
